471892d0ffd66d20b3807a045a53e0bfa404fdbd
Sam Hedges <sam.hedges.sh@gmail.com>
2025-10-08 22:20:30 +0100

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,6 +6,24 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
                      LPSTR CommandLine, int ShowCode) {
 
     OutputDebugStringA("Hello World!\n");
+
+    char SmallS;
+    char unsigned SmallU;
+
+    short MediumS;
+    short unsigned MediumU;
+
+    int LargeS;
+    int unsigned LargeU;
+
+    char unsigned Test;
+
+    Test = 255;
+
+    Test += 1;
+
+    Test -= 1;
+
     return 0;
 }
 
