f187f1ffa31fa9f88e8f4e696b66ff35b47208ca
Sam Hedges <sam.hedges.sh@gmail.com>
2025-10-15 10:06:06 +0100

    Started Creating Basic Window for Windows

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,75 @@
 #include <iostream>
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode) {
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
 
-	MessageBox(0, "This is a Handmade Game.", "Handmade Game", MB_OK | MB_ICONINFORMATION);
+	LRESULT Result = 0;
+
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		}
+		break;
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		}
+		break;
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+		}
+		break;
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		}
+		break;
+		default:
+		{
+			// OutputDebugStringA("default\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		}
+		break;
+	}
+
+	return (Result);
+}
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode)
+{
+
+	WNDCLASS WindowClass = {};
+
+	// TODO(Sam): Check if OWNDC, HREDRAW and VREDRAW still matter
+	WindowClass.style		  = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc	  = MainWindowCallback;
+	WindowClass.hInstance	  = Instance;
+	WindowClass.lpszClassName = "HandmadeGameWindowClass";
+	// WindowClass.icon		  = ;
+
+	if (RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle =
+			CreateWindowEx(0, WindowClass.lpszClassName, "Handmade Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+						   CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
+		if (WindowHandle)
+		{
+			MSG Message;
+			GetMessage(&Message, 0, 0, 0);
+		}
+		else
+		{
+			// TODO(Sam): Logging fail state
+		}
+	}
+	else
+	{
+		// TODO(Sam): Logging fail state
+	}
 
 	return 0;
 }
