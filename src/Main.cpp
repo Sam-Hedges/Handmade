26f14a5761cb5f09069de39f6e75ce811599d164
Sam Hedges <sam.hedges.sh@gmail.com>
2025-10-18 00:58:51 +0100

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,8 +10,6 @@ global_var bool Running;
 
 global_var BITMAPINFO BitmapInfo;
 global_var void *BitmapMemory;
-global_var HBITMAP BitmapHandle;
-global_var HDC BitmapDeviceContext;
 
 // Device Independent Bitmap
 internal void ResizeDIBSection(int Width, int Height)
@@ -19,15 +17,9 @@ internal void ResizeDIBSection(int Width, int Height)
 	// TODO(Sam): Bulletproof this.
 	// Maybe don't free first, free after, then free first if that fails.
 
-	if(BitmapHandle)
+	if (BitmapMemory)
 	{
-		DeleteObject(BitmapHandle);
-	}
-
-	if(!BitmapDeviceContext)
-	{
-		// TODO(Sam): Should we recreate these under certain special circumstances.
-		BitmapDeviceContext = CreateCompatibleDC(0);
+		VirtualFree(BitmapMemory, 0, MEM_RELEASE);
 	}
 
 	BitmapInfo.bmiHeader.biSize		   = sizeof(BitmapInfo.bmiHeader);
@@ -37,21 +29,22 @@ internal void ResizeDIBSection(int Width, int Height)
 	BitmapInfo.bmiHeader.biBitCount	   = 32;
 	BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-	BitmapHandle =
-		CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+	int BytesPerPixel	 = 4;
+	int BitmapMemorySize = (Width * Height) * BytesPerPixel;
+	BitmapMemory		 = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
 internal void BlitWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
 {
-	StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height, BitmapMemory,
-				  &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+	StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height, BitmapMemory, &BitmapInfo, DIB_RGB_COLORS,
+				  SRCCOPY);
 }
 
 LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
 	LRESULT Result = 0;
 
-	switch(Message)
+	switch (Message)
 	{
 		case WM_SIZE:
 		{
@@ -116,20 +109,20 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 	WindowClass.lpszClassName = "HandmadeGameWindowClass";
 	// WindowClass.icon		  = ;
 
-	if(RegisterClassA(&WindowClass))
+	if (RegisterClassA(&WindowClass))
 	{
-		HWND WindowHandle = CreateWindowExA(
-			0, WindowClass.lpszClassName, "Handmade Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
+		HWND WindowHandle =
+			CreateWindowExA(0, WindowClass.lpszClassName, "Handmade Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+							CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
 
-		if(WindowHandle)
+		if (WindowHandle)
 		{
 			Running = true;
-			while(Running)
+			while (Running)
 			{
 				MSG Message;
 				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
-				if(MessageResult > 0)
+				if (MessageResult > 0)
 				{
 					TranslateMessage(&Message);
 					DispatchMessageA(&Message);
